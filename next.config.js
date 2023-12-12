4e801a95914f26ce79dbf386e2d4752fbcdc2bb4
muhammeddilaver <muhammed.dilaver@gmail.com>
2023-12-12 00:50:47 -0500

    Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    incrementalCacheHandlerPath: require.resolve('./cache-handler.js'),
-    isrMemoryCacheSize: 0, // disable default in-memory caching
-  },
-  output: 'export',
-  images: {
-    loader: 'custom',
-    loaderFile: './my-loader.ts',
-  },
-    reactStrictMode: true,
-    swcMinify: true,
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
