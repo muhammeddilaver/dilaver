aefa92d320115ea0ee0ad6f043becf9fd7086f29
muhammeddilaver <muhammed.dilaver@gmail.com>
2023-12-13 12:03:22 -0500

    Update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Techno Yapı Online - Restful API',
-    description: `"Techno Yapı Online - Restful API" has been specifically crafted to address the order management needs of the business, tailored entirely to the requirements of the enterprise. Leveraging technologies such as NodeJS, Express.js, JWT, and MongoDB, this solution is designed to provide efficient and scalable order management capabilities. The use of a Restful API ensures seamless communication between different components, promoting flexibility and interoperability within the system. This API enables streamlined interactions for order processing, enhancing the overall efficiency of the business operations.`,
+    description: `has been specifically crafted to address the order management needs of the business, tailored entirely to the requirements of the enterprise. Leveraging technologies such as NodeJS, Express.js, JWT, and MongoDB, this solution is designed to provide efficient and scalable order management capabilities. The use of a Restful API ensures seamless communication between different components, promoting flexibility and interoperability within the system. This API enables streamlined interactions for order processing, enhancing the overall efficiency of the business operations.`,
     imgSrc: '/images/members-only.png',
     live: 'https://technoapidocker.onrender.com/auth/me',
     github: 'https://github.com/muhammeddilaver/techno-yapi-online-api',
@@ -15,7 +15,8 @@ const projectsData = [
   },
   {
     title: 'Techno Yapı Online - FrontEnd',
-    description: `"Techno Yapı Online - Restful API" has been specifically crafted to address the order management needs of the business, tailored entirely to the requirements of the enterprise. Leveraging technologies such as NodeJS, Express.js, JWT, and MongoDB, this solution is designed to provide efficient and scalable order management capabilities. The use of a Restful API ensures seamless communication between different components, promoting flexibility and interoperability within the system. This API enables streamlined interactions for order processing, enhancing the overall efficiency of the business operations.`,
+    description: `has been meticulously crafted to address the business's order management requirements, entirely tailored to the specific needs of the enterprise. Utilizing technologies such as React.js, Bootstrap, JWT, Ant Media, Axios, Formik, React Query, React Router DOM, and Redis, this front-end solution is designed to provide a robust and user-friendly interface for effective order management. The integration of these technologies ensures a seamless and responsive user experience, allowing for efficient interaction with the order management system.
+    `,
     imgSrc: '/images/members-only.png',
     live: 'https://technoyapi.online',
     github: 'https://github.com/muhammeddilaver/technoyapi-online-frontend',
